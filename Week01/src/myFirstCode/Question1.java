639cbc729bc257f0d0b25aff03b7c714cef36d10
YoungBDa <YoungBDa@host.docker.internal>
2025-06-10 18:31:50 -0600

    helloworld, question1

---
+++ Week01/src/myFirstCode/Question1.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		/*
+		 * 1.	Write a program that will read in two integers typed at the keyboard, subtract one from the other, and then display the result.
+		 */
+		// TODO Auto-generated method stub
+		int firstNumber;
+		int secondNumber;
+		int Result;
+		
+		firstNumber = JOptionPane.showInputDialog("Enter a Number");
+		secondNumber = JOptionpane.showInputDialog("enter a number to Subtract");
+		Result = firstNumber - secondNumber;
+		
+
+	}
+
+}
