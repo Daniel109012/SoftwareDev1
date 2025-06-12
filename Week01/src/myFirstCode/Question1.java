41405d4f9d99ab293a51e52c00f61df0aa6fd08a
Daniel109012 <Daniel_101290@yahoo.com>
2025-06-11 21:56:58 -0600

    Question1

--- Week01/src/myFirstCode/Question1.java
+++ Week01/src/myFirstCode/Question1.java
@@ -4,8 +4,31 @@ import javax.swing.JOptionPane;
 
 public class Question1 {
     public static void main(String[] args) {
+    	
+    	/*
+    	 * 1.	Write a program that will read in two integers typed at the keyboard, subtract one from the other, and then display the result.
+    	 */
+    	
+    	String firstNum;
+    	String secNum;
+    	String Output;
+    	int Num1;
+    	int Num2;
+    	int result;
+    	
+    	firstNum = JOptionPane.showInputDialog("Enter a Number from which to subtract");
+    	secNum = JOptionPane.showInputDialog("Enter Number to subtract");
+    	
+    	Num1 = Integer.parseInt(firstNum); 
+    	Num2 = Integer.parseInt(secNum); 
+    	
+    	result = Num1 - Num2;
+    	
+    	Output = "The Difference of " + Num1 + " and " + Num2 + " is " + result;
     	 
-        JOptionPane.showMessageDialog(null, "Hello, World!");
+    	JOptionPane.showMessageDialog(null, Output, "Result",
+    			JOptionPane.PLAIN_MESSAGE);
+        
     }
 
 }
