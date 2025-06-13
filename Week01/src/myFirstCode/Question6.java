b7b3f7b532574420439fe75941964fe9724f7526
Daniel109012 <Daniel_101290@yahoo.com>
2025-06-12 23:33:55 -0600

    Question7

--- Week01/src/myFirstCode/Question6.java
+++ Week01/src/myFirstCode/Question6.java
@@ -3,7 +3,10 @@ package myFirstCode;
 public class Question6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		 * 6.	Write a program that will accept a temperature in degrees Celsius and output the equivalent in degrees Fahrenheit. 
+		 * The formula for the conversion is F = (C * 9.0/5.0) + 32. You should make the variables that you use of type float.
+		 */
 
 	}
 
