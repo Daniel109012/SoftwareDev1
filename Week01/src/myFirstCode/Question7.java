b66cf8149b54116e9bed52fcb3bf21034c81d30d
Daniel109012 <Daniel_101290@yahoo.com>
2025-06-12 23:33:46 -0600

    Question7

--- Week01/src/myFirstCode/Question7.java
+++ Week01/src/myFirstCode/Question7.java
@@ -3,7 +3,14 @@ package myFirstCode;
 public class Question7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		 * 7.	Write a program that produces the following output.
+		 * The backslash character (\) is easy to print
+		 * The double quotes  are also “easy” to print
+		 */
+		
+		System.out.println("The backslash character (\\) is easy to print.");
+		System.out.println("The double quotes (\"\") are also “easy” to print.");
 
 	}
 
