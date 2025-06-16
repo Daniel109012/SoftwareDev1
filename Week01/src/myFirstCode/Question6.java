8acca1f9cabbb3ed9f333358fb6711c4ff5cad4e
Daniel109012 <Daniel_101290@yahoo.com>
2025-06-15 22:31:00 -0600

    Celsius to Fahrenheit

--- Week01/src/myFirstCode/Question6.java
+++ Week01/src/myFirstCode/Question6.java
@@ -1,5 +1,7 @@
 package myFirstCode;
 
+import javax.swing.JOptionPane;
+
 public class Question6 {
 
 	public static void main(String[] args) {
@@ -7,6 +9,25 @@ public class Question6 {
 		 * 6.	Write a program that will accept a temperature in degrees Celsius and output the equivalent in degrees Fahrenheit. 
 		 * The formula for the conversion is F = (C * 9.0/5.0) + 32. You should make the variables that you use of type float.
 		 */
+		
+		String Input;
+		String Output;
+		
+		Input = JOptionPane.showInputDialog("Enter Degree in Celsius to convert to Fahrenheit");
+		
+		float Celsius;
+		Celsius = Float.parseFloat(Input);
+		
+		float Fahrenheit;
+		float div;
+		div = (float) (9.0/5.0);
+		Fahrenheit = (Celsius * div) + 32;
+		
+		Output = "The Temperature in Fahrenheit is " + Fahrenheit;
+		
+		JOptionPane.showMessageDialog(null, Output);
+		
+		
 
 	}
 
