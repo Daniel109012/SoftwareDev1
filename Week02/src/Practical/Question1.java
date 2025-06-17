0fb790c77c5bd4971f1b3977cc30881be53648c0
Daniel109012 <Daniel_101290@yahoo.com>
2025-06-16 22:38:04 -0600

    Question1

---
+++ Week02/src/Practical/Question1.java
@@ -0,0 +1,14 @@
+package Practical;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		/*1.	Write a program that reads in two integers from the keyboard and displays: 
+		•		the value of them added together 
+		•		the value of them multiplied together. 
+		The program should also print a message saying which of the numbers is the largest. Make sure that your program can handle the numbers if they are equal.
+		 */
+
+	}
+
+}
