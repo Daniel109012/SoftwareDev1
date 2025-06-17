4d01be651d9c135a20500d712494f904d3e91af0
YoungBDa <YoungBDa@WS08061X02.mshome.net>
2025-06-17 11:58:28 -0600

    ternary

--- Week02/src/Samples/ternaryOperator.java
+++ Week02/src/Samples/ternaryOperator.java
@@ -3,7 +3,23 @@ package Samples;
 public class ternaryOperator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	    // declare variables
+		int mark  = 66;
+		int passMark = 50;
+		String result;
+	    
+		// test to see if the mark is a pass or not
+		/*if(mark >= passMark) {
+			System.out.println("mark = " +mark);
+	    	result = "Pass";
+	    } else {
+	    	result = "Fail";
+	    }*/
+	    
+		result = (mark >= passMark) ? "Pass" : "Fail"; 
+		
+		//JOptionPane.showMessageDialog(null, output);
+	    System.out.println(result);
 
 	}
 
