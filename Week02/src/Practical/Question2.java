4d94ace8b0c829a61a7af00c024330623bffb1d8
YoungBDa <YoungBDa@WS08061X02.mshome.net>
2025-06-18 11:21:59 -0600

    Question2 = using Modulus

---
+++ Week02/src/Practical/Question2.java
@@ -0,0 +1,27 @@
+package Practical;
+
+import javax.swing.JOptionPane;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		/*
+		 * 2.	Write a program that reads in an integer then determines and prints whether it is odd or even.
+		 */
+		String Input;
+		String Output;
+		int Num;
+		
+		
+		Input = JOptionPane.showInputDialog("Eenter a Number");
+		Num = Integer.parseInt(Input);
+		
+		if (Num % 2 == 0 ) {
+			Output = Num + " Is an Even Number";
+		}else {
+			Output = Num + " Is an Odd Number";
+		}
+		
+		JOptionPane.showMessageDialog(null, Output);
+	}
+}
