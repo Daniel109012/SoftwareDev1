7df33519ba26f13684133740d39dbaf0b4d3f3f7
YoungBDa <YoungBDa@WS08061X02.mshome.net>
2025-06-18 11:23:50 -0600

    Question3

--- Week02/src/Practical/Question3.java
+++ Week02/src/Practical/Question3.java
@@ -3,7 +3,10 @@ package Practical;
 public class Question3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		 * 3.	Write a program that will test whether a character entered is a vowel or not and will display an appropriate message as a result.  
+		 * Here is the start of the program up to reading in and converting to a character, after that you need to test if it is a vowel or not.
+		 */
 
 	}
 
