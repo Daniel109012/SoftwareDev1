af304dc3747db30cd8bd58e1266ea330bc2ba3dd
Daniel109012 <Daniel_101290@yahoo.com>
2025-06-18 21:48:04 -0600

    Question4

--- Week02/src/Practical/Question3.java
+++ Week02/src/Practical/Question3.java
@@ -8,6 +8,13 @@ public class Question3 {
 		/*
 		 * 3.	Write a program that will test whether a character entered is a vowel or not and will display an appropriate message as a result.  
 		 * Here is the start of the program up to reading in and converting to a character, after that you need to test if it is a vowel or not.
+		 * 
+		 * String output;
+		 *String charAsString;
+		 *Char theChar;
+
+		charAsString = JOptionPane.showInputDialog("please enter a character");
+		theChar = charAsString.charAt(0);
 		 */
 		
 		String Input;
