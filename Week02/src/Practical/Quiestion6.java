904f3d1ea4c455b51658330c10ae038a055de2da
YoungBDa <YoungBDa@WS08061X02.mshome.net>
2025-06-19 10:38:23 -0600

    Question 6 & 7

---
+++ Week02/src/Practical/Quiestion6.java
@@ -0,0 +1,12 @@
+package Practical;
+
+public class Quiestion6 {
+
+	public static void main(String[] args) {
+		/*
+		 * 6.   Write a program that inputs a series of 10 numbers and determines and prints the largest of the numbers and the smallest of the numbers.
+		 */
+
+	}
+
+}
