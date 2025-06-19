dc647e5dc151453c0043b95e84588ce861642248
Daniel109012 <Daniel_101290@yahoo.com>
2025-06-18 22:53:16 -0600

    Question4

--- Week02/src/Practical/Question4.java
+++ Week02/src/Practical/Question4.java
@@ -1,10 +1,41 @@
 package Practical;
 
+import javax.swing.JOptionPane;
+
 public class Question4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		 * 4.	Develop a program that will calculate the pay for children who deliver newspapers.  The pay is calculated as follows: 
 
+		1 – 100 copies   10 pence per paper
+		101 or more      15  pence per paper
+		For example, if somebody delivers 105 papers then their pay would be
+		(100 x 10) + (5 x 15) = 1075 pence or 10 pounds and 75 pence
+
+		Write a program that asks for the number of papers delivered and calculates and displays the amount earned.
+		 */
+
+		String Input;
+		String Output = "";
+		int TPence;
+		int Copies;
+		
+		Input = JOptionPane.showInputDialog("enter the amount of newspapers Copies sold");
+		Copies = Integer.parseInt(Input);
+		
+		if(Copies <= 100) {
+			TPence = Copies * 10;
+			Output = "The amount earned is " + TPence;
+		} else {
+			if(Copies > 100) {
+			TPence = (Copies - 100) * 15 + (100 * 10);
+			Output = "The amount earned is " + TPence;
+			}
+		}
+		
+		JOptionPane.showMessageDialog(null, Output);
+		
 	}
 
 }
