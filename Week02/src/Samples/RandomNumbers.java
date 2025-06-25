3b1292a366ac1530d23fc76d8f647424a7ace1a1
YoungBDa <YoungBDa@WS08061X02.mshome.net>
2025-06-25 12:35:54 -0600

    removed warnings

--- Week02/src/Samples/RandomNumbers.java
+++ Week02/src/Samples/RandomNumbers.java
@@ -1,11 +1,11 @@
 package Samples;
 
-import javax.swing.JOptionPane;
+
 import java.util.Random;
 
 public class RandomNumbers {
 	public static void main(String[] args) {
-		String output;
+		
 		int num;
 		Random numGenerator = new Random();  //don't worry about that
 		
