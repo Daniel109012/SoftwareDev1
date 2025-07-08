18d3c721c097f8c4a9ca1457eaebf76bcb6c3b55
YoungBDa <YoungBDa@WS08061X02.mshome.net>
2025-07-08 10:07:45 -0600

    Question06 complete

--- Week05/src/practice/Question06.java
+++ Week05/src/practice/Question06.java
@@ -1,5 +1,9 @@
 package practice;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
 public class Question06 {
 
 	public static void main(String[] args) {
@@ -10,6 +14,36 @@ public class Question06 {
 		 * you only need to print the total number of heads and the total number of tails.
 		 */
 
-	}
+		coinFlip();
+		
+	}//end main
+	
+	public static void coinFlip() {
+		
+		String output = "";
+		int coin;
+		int headsCount = 0;
+		int tailsCount = 0;
+		Random flip = new Random();
+		
+		
+		for(int count = 0; count < 1000; count++) {
+			coin = flip.nextInt(2);
+			
+			
+			if(coin == 1) {
+				headsCount++;
+			}else {
+				tailsCount++;
+			}
+			
+		}
 
-}
+		output = output + "\n number of Heads Count " + headsCount;
+		output = output + "\n number of Tails Count " + tailsCount;
+		
+		JOptionPane.showMessageDialog(null,output);
+		
+		
+	}
+}//end class
