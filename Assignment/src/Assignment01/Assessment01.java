41da26279cbefdf995938dbb806f07d5d466b3c3
Daniel109012 <Daniel_101290@yahoo.com>
2025-07-12 17:34:05 -0600

    table data

--- Assignment/src/Assignment01/Assessment01.java
+++ Assignment/src/Assignment01/Assessment01.java
@@ -1,50 +1,16 @@
 package Assignment01;
 
-import java.util.Random;
+//import java.util.Random;
 
-import javax.swing.JOptionPane;
+//import javax.swing.JOptionPane;
 
 public class Assessment01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		
-
-		String rowAsString;
-		String columnAsString;
-		String output;
-		int N ;
-		int M ;
+		assessment01Methods.grid(); //calls void grid to display the table
 		
-		Random randomNum = new Random(); 
-		
-		do{
-			rowAsString = JOptionPane.showInputDialog("Enter the number of Rows between 3 and 10");
-		N = Integer.parseInt(rowAsString);
-		}while(3 > N || 10 < N);
-		
-		do{
-		columnAsString = JOptionPane.showInputDialog("Enter the number or Columns");
-		M = Integer.parseInt(columnAsString);
-		}while(3 > M || 10 < M);
-		
-		int[][] grid = new int[N][M];
-
-		
-		output = (" Assignment One \n\n");
-		
-		for(int r = 0; r < N; r++) {
-			for(int c = 0; c < M; c++) {
-				grid[r][c] = randomNum.nextInt(1000);//generate random numbers from 0 - 1000
-				
-				output = output + ("   "+ grid[r][c]);
-			}
-			
-			output += "\n"; // Add a newline after each row
-		}
-		
-			JOptionPane.showMessageDialog(null, output);
 		
 	}//end main
 
--- Assignment/src/Assignment01/assessment01Methods.java
+++ Assignment/src/Assignment01/assessment01Methods.java
@@ -2,55 +2,101 @@ package Assignment01;
 
 import javax.swing.*;
 import java.util.Random;
-import javax.swing.table.*;
-import java.awt.*;
 
 public class assessment01Methods {
 
-	public static void grid(int N, int M) {
+	public static void grid() {
 		
 		
-		//int[] rowNames = grid;
-		String rowAsString;
-		String columnAsString;
+		String rowAsString; //input number of rows in a string
+		String columnAsString; // input number of columns in a string
+		int N ; //rows
+		int M ; //columns
+		
+		
+		do{
+			rowAsString = JOptionPane.showInputDialog("Enter the number of Rows between 3 and 10");
+		N = Integer.parseInt(rowAsString); // convert string to Integer for number of rows
+		}while(N < 3 || N > 10); // only accepts values between 3 and 10
+		
+		do{
+		columnAsString = JOptionPane.showInputDialog("Enter the number or Columns  between 3 and 10");
+		M = Integer.parseInt(columnAsString);  // convert string to Integer for number of columns
+		}while(M < 3 || M > 10); // only accepts values between 3 and 10
+		
+		int[][] grid = gridNumbers(N , M); // calls random numbers from the above input
+
+		assessment01Methods.table(grid); // calls the table to input the data
+
+	} // end grid method
+	
+	
+	//--------------------------------------------------------------------------------------
+	
+	
+	public static int[][] gridNumbers(int N, int M){
+		
+		// collect random numbers based on the user input from above
+		
+		int[][] grid = new int[N][M]; // 2D array for table
+		Random randomNum = new Random(); 
 		String output;
-		int[][] grid = new int[N][M];
-		Random randomNum = new Random();
+
 		
+		output = (" Assignment One \n\n");
 		
-		//Input number of Rows
-		rowAsString = JOptionPane.showInputDialog("Enter the number of Rows");
-		N = Integer.parseInt(rowAsString);
-		
-		//Enter number of Columns
-		columnAsString = JOptionPane.showInputDialog("Enter the number or Columns");
-		M = Integer.parseInt(columnAsString);
-		
-		output = " Assignment One";
-		
-		//generate random numbers in 2d array
-		for(int r = 0; r < N; r++) {
-			for(int j = 0; j < M; j++) {
-				grid[r][j] = randomNum.nextInt(1001);//generate random numbers from 0 - 1000
-				output = output + grid[r][j];
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c < M; c++) {
+				if(r %2 == 0) {
+					grid[r][c] = 2 * randomNum.nextInt(100) + 1;//generate random numbers from 1 - 1000
+					
+				}else {
+					grid[r][c] = 1 + 2 * randomNum.nextInt(100) + 1;//generate random numbers from 1 - 1000
+				}
+			
 			}
-			output = output + "\n";
+			
 		}
 		
-		//print out the random numbers
+		JOptionPane.showMessageDialog(null, output);
 		
-	/*	for(int r = 0; r < N; r++) {
-			for(int j = 0; j < M; j++) {
+		return grid; //returns the randome numbrs 
+		
+	}//exit gridNumbers method
+	
+	
+	//--------------------------------------------------------------------------------------
+	
+	
+	public static void table(int[][] grid) {
+		
+		//to display the table
+		
+		int N = grid.length;
+		int M = grid[0].length;
+		
+		Object[][] matrix = new Object[N][M];
+		for(int r = 0; r < N; r++) {
+			for(int c = 0; c < M; c++) {
+				matrix[r][c] = grid[r][c];
 				
-				output = output + grid[r][j] + "\t" + "\n";
-			}
-			
-			
-		}*/
+			}//exit for
+		}//end for
 		
-		JOptionPane.showMessageDialog(null, output, "assignment01", JOptionPane.INFORMATION_MESSAGE);
+		String[] MName = new String[M];
+		for(int c = 0; c < M; c++) {
+			MName[c] = "C" + (c + 1);
+		}//exit column nameing for
 		
-
-	} // end grid
+		JTable table = new JTable(matrix, MName);
+		JScrollPane scrollPane = new JScrollPane(table);
+		JOptionPane.showMessageDialog(null, scrollPane, "Assignment One", JOptionPane.INFORMATION_MESSAGE);
+		
+	}//exit table method	
+	
+	
+	//-------------------------------------------------------------------------------------
 
 }//end class
+
+
