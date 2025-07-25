d960b68c5c2e9481dc2582771d87cfca0b03f60e
YoungBDa <YoungBDa@host.docker.internal>
2025-07-25 14:50:08 -0600

    update

--- Week07/src/practicals/DemoPerson.java
+++ Week07/src/practicals/DemoPerson.java
@@ -7,8 +7,15 @@ public class DemoPerson {
 		
 		
 		Person p1 = new Person();
+		
 		Person[] p2 = new Person[3];
-
+		p2[0] = new Person("John", 21, "Chicago");
+		p2[1] = new Person("Alise", 25, "LA");
+		p2[2] = new Person ("Robert", 40, "Belize");
+		
+		for(int i = 0; i < p2.length; i++) {
+		p2[i].printDetails();
+		}
 		
 		p1.printDetails();
 
--- Week07/src/practicals/Person.java
+++ Week07/src/practicals/Person.java
@@ -20,6 +20,7 @@ public class Person {
 		
 	}
 	
+	
 	public String happyBirthday() {
 		
 		String output;
@@ -30,7 +31,10 @@ public class Person {
 		
 	}
 	
+	
+	
 	public Person(String name, int age, String address) {
+//		=========Array=======		//
 		
 		this.name = name;
 		this.age =  age;
@@ -40,6 +44,8 @@ public class Person {
 	
 	public void printDetails() {
 		
+//		=========output Details=========
+		
 		String output;
 		output = toString();
 		JOptionPane.showMessageDialog(null, output);
@@ -48,5 +54,31 @@ public class Person {
 
 	}
 	
+	
+//	============get and set methods========
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAddress() {
+		return this.address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
 
 }
